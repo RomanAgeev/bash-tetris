e794fdc7aa402c48d009d287755a90757df0a8ae
Roman Ageev <romanalag@gmail.com>
2024-07-18 23:17:25 +0200

    implement shape rotation

--- main.sh
+++ main.sh
@@ -17,9 +17,9 @@ new_shape shape_s ".xx xx."
 clear
 
 render_shape shape_o 10 10
-render_shape shape_z 10 15
-render_shape shape_j 10 20
+render_shape shape_z 10 15 1
+render_shape shape_j 10 20 2
 render_shape shape_i 10 25
-render_shape shape_t 10 31
-render_shape shape_l 10 36
+render_shape shape_t 10 31 3
+render_shape shape_l 10 36 6
 render_shape shape_s 10 41
--- shape.sh
+++ shape.sh
@@ -13,6 +13,69 @@ get_shape_default_placeholder() {
     get_field "$shape_classname" default_placeholder
 }
 
+_shape_format() {
+    __SHAPE_FORMAT=()
+    for line in "$@"; do
+        local line_format="${line//[^.]/%s}"
+        line_format="${line_format//./ %.0s}"
+        __SHAPE_FORMAT+=( "$line_format" )
+    done
+}
+
+_shape_lines_1() {
+    local width="${1:?}"
+    local height="${2:?}"
+
+    shift 2
+
+    local lines=( $@ )
+
+    __SHAPE_LINES_1=()
+    for (( i=0; i<$width; i++ )); do
+        local line=""
+        for (( j=$height-1; j>=0; j-- )); do
+            line=$line"${lines[$j]:$i:1}"
+        done
+        __SHAPE_LINES_1+=( "$line" )
+    done
+}
+
+_shape_lines_2() {
+    local width="${1:?}"
+    local height="${2:?}"
+
+    shift 2
+
+    local lines=( $@ )
+
+    __SHAPE_LINES_2=()
+    for (( j=$height-1; j>=0; j-- )); do
+        local line=""
+        for (( i=$width-1; i>=0; i-- )); do
+            line=$line"${lines[$j]:$i:1}"
+        done
+        __SHAPE_LINES_2+=( "$line" )
+    done
+}
+
+_shape_lines_3() {
+    local width="${1:?}"
+    local height="${2:?}"
+
+    shift 2
+
+    local lines=( $@ )
+
+    __SHAPE_LINES_3=()
+    for (( i=$width-1; i>=0; i-- )); do
+        local line=""
+        for (( j=0; j<$height;j++ )); do
+            line=$line"${lines[$j]:$i:1}"
+        done
+        __SHAPE_LINES_3+=( "$line" )
+    done
+}
+
 new_shape() {
     local this="${1:?}"
     local string="${2:?}"
@@ -23,25 +86,39 @@ new_shape() {
     IFS=' ' read -r -a lines <<< "$string"
 
     local width
-    local format=()
     for line in "${lines[@]}"; do
         local line_width="${#line}"
         if [ $line_width -ne ${width:=$line_width} ]; then
             printf "Shape \"%s\"is not rectangular\n" "$string" >&2
             return 1
         fi
-        local line_format="${line//[^.]/%s}"
-        line_format="${line_format//./ %.0s}"
-        format+=( "$line_format" )
     done
 
     local height="${#lines[@]}"
+
+    # 0-rotation format
+    _shape_format "${lines[@]}"
+    set_array_field "$this" format_0 "${__SHAPE_FORMAT[@]}"
+
+    # 90-rotation format
+    _shape_lines_1 "$width" "$height" "${lines[@]}"
+    _shape_format "${__SHAPE_LINES_1[@]}"
+    set_array_field "$this" format_1 "${__SHAPE_FORMAT[@]}"
+
+    # 180-rotation format
+    _shape_lines_2 "$width" "$height" "${lines[@]}"
+    _shape_format "${__SHAPE_LINES_2[@]}"
+    set_array_field "$this" format_2 "${__SHAPE_FORMAT[@]}"
+
+    # 270-rotation format
+    _shape_lines_3 "$width" "$height" "${lines[@]}"
+    _shape_format "${__SHAPE_LINES_3[@]}"
+    set_array_field "$this" format_3 "${__SHAPE_FORMAT[@]}"
 
     fill_array $(( $width * $height )) "$placeholder"
 
     set_field "$this" width "$width"
     set_field "$this" height "$height"
-    set_array_field "$this" format "${format[@]}"
     set_array_field "$this" array "${__FILL_ARRAY[@]}"
 }
 
@@ -49,8 +126,13 @@ render_shape() {
     local this="${1:?}"
     local row="${2:?}"
     local col="${3:?}"
+    local rotation=$(( ${4:-0} % 4 ))
 
-    local format; get_array_field "$this" format
+    local format_field="format_${rotation}"
+
+    eval "local $format_field; get_array_field \$this $format_field"
+    eval "local format=( \"\${$format_field[@]}\" )"
+
     local array; get_array_field "$this" array
 
     local canvas="${this}_canvas"
